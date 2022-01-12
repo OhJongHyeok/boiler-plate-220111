557315ca28c8a3ee21014a2e316038ab8505dd22
오종혁 <whdgur4817@DESKTOP-6B67QHD.localdomain>
2022-01-12 18:09:21 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key')
+const { User } = require('./models/Users');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://whdgur4817:whdgur2422%40@boilerplate-shard-00-00.vvrdw.mongodb.net:27017,boilerplate-shard-00-01.vvrdw.mongodb.net:27017,boilerplate-shard-00-02.vvrdw.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-ug9q92-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     //useNewUrlParse : true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected.."))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~안녕하세요')
+  res.send('Hello World! 123')
+})
+
+app.post('/register', (req,res) => {
+
+  //회원 가입 할 때 필요한 정보들을 Client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다. 즉, 이 스크립트에서는 우리가 Users에서 가져온 정보들을 데이터 베이스에 넣어주는 것
+  //req.body안에 id, pw와 같은 정보들이 있어서 
+
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
